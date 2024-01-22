b2ffbf6a360ce76d8cded118d13ec774b3c31236
Peter.Hsieh <peter.hsieh@ecloudvalley.com>
2024-01-22 15:04:54 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Amplify } from 'aws-amplify';
-
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
